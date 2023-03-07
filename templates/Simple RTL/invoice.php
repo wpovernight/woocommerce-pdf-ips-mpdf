f286d591d20f5339f0e91d4a66007f5ecb4ff75d
Alexandre Faustino <alexmigf@gmail.com>
2023-03-07 12:19:44 +0000

    Fix: bug on footer not fixed in bottom on Simple mPDF and RTL templates
    #7

--- templates/Simple RTL/invoice.php
+++ templates/Simple RTL/invoice.php
@@ -1,41 +1,49 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
-<?php do_action( 'wpo_wcpdf_before_document', $this->type, $this->order ); ?>
+<?php do_action( 'wpo_wcpdf_before_document', $this->get_type(), $this->order ); ?>
 
-<table class="head container">
-	<tr>
-		<td class="header">
-		<?php
-		if( $this->has_header_logo() ) {
-			$this->header_logo();
-		} else {
-			echo $this->get_title();
-		}
-		?>
-		</td>
-		<td class="shop-info">
-			<?php do_action( 'wpo_wcpdf_before_shop_name', $this->type, $this->order ); ?>
-			<div class="shop-name"><h3><?php $this->shop_name(); ?></h3></div>
-			<?php do_action( 'wpo_wcpdf_after_shop_name', $this->type, $this->order ); ?>
-			<?php do_action( 'wpo_wcpdf_before_shop_address', $this->type, $this->order ); ?>
-			<div class="shop-address"><?php $this->shop_address(); ?></div>
-			<?php do_action( 'wpo_wcpdf_after_shop_address', $this->type, $this->order ); ?>
-		</td>
-	</tr>
-</table>
+<htmlpageheader name="docHeader">
+	<table class="head container">
+		<tr>
+			<td class="header">
+			<?php
+			if ( $this->has_header_logo() ) {
+				do_action( 'wpo_wcpdf_before_shop_logo', $this->get_type(), $this->order );
+				$this->header_logo();
+				do_action( 'wpo_wcpdf_after_shop_logo', $this->get_type(), $this->order );
+			} else {
+				echo $this->get_title();
+			}
+			?>
+			</td>
+			<td class="shop-info">
+				<?php do_action( 'wpo_wcpdf_before_shop_name', $this->get_type(), $this->order ); ?>
+				<div class="shop-name"><h3><?php $this->shop_name(); ?></h3></div>
+				<?php do_action( 'wpo_wcpdf_after_shop_name', $this->get_type(), $this->order ); ?>
+				<?php do_action( 'wpo_wcpdf_before_shop_address', $this->get_type(), $this->order ); ?>
+				<div class="shop-address"><?php $this->shop_address(); ?></div>
+				<?php do_action( 'wpo_wcpdf_after_shop_address', $this->get_type(), $this->order ); ?>
+			</td>
+		</tr>
+	</table>
+</htmlpageheader>
+
+<div class="bottom-spacer"></div>
+
+<?php do_action( 'wpo_wcpdf_before_document_label', $this->get_type(), $this->order ); ?>
 
 <h1 class="document-type-label">
-<?php if( $this->has_header_logo() ) echo $this->get_title(); ?>
+	<?php if ( $this->has_header_logo() ) echo $this->get_title(); ?>
 </h1>
 
-<?php do_action( 'wpo_wcpdf_after_document_label', $this->type, $this->order ); ?>
+<?php do_action( 'wpo_wcpdf_after_document_label', $this->get_type(), $this->order ); ?>
 
 <table class="order-data-addresses">
 	<tr>
 		<td class="address billing-address">
 			<!-- <h3><?php _e( 'Billing Address:', 'woocommerce-pdf-invoices-packing-slips' ); ?></h3> -->
-			<?php do_action( 'wpo_wcpdf_before_billing_address', $this->type, $this->order ); ?>
+			<?php do_action( 'wpo_wcpdf_before_billing_address', $this->get_type(), $this->order ); ?>
 			<?php $this->billing_address(); ?>
-			<?php do_action( 'wpo_wcpdf_after_billing_address', $this->type, $this->order ); ?>
+			<?php do_action( 'wpo_wcpdf_after_billing_address', $this->get_type(), $this->order ); ?>
 			<?php if ( isset($this->settings['display_email']) ) { ?>
 			<div class="billing-email"><?php $this->billing_email(); ?></div>
 			<?php } ?>
@@ -46,14 +54,14 @@
 		<td class="address shipping-address">
 			<?php if ( !empty($this->settings['display_shipping_address']) && ( $this->ships_to_different_address() || $this->settings['display_shipping_address'] == 'always' ) ) { ?>
 			<h3><?php _e( 'Ship To:', 'woocommerce-pdf-invoices-packing-slips' ); ?></h3>
-			<?php do_action( 'wpo_wcpdf_before_shipping_address', $this->type, $this->order ); ?>
+			<?php do_action( 'wpo_wcpdf_before_shipping_address', $this->get_type(), $this->order ); ?>
 			<?php $this->shipping_address(); ?>
-			<?php do_action( 'wpo_wcpdf_after_shipping_address', $this->type, $this->order ); ?>
+			<?php do_action( 'wpo_wcpdf_after_shipping_address', $this->get_type(), $this->order ); ?>
 			<?php } ?>
 		</td>
 		<td class="order-data">
 			<table>
-				<?php do_action( 'wpo_wcpdf_before_order_data', $this->type, $this->order ); ?>
+				<?php do_action( 'wpo_wcpdf_before_order_data', $this->get_type(), $this->order ); ?>
 				<?php if ( isset($this->settings['display_number']) ) { ?>
 				<tr class="invoice-number">
 					<th><?php echo $this->get_number_title(); ?></th>
@@ -78,13 +86,13 @@
 					<th><?php _e( 'Payment Method:', 'woocommerce-pdf-invoices-packing-slips' ); ?></th>
 					<td><?php $this->payment_method(); ?></td>
 				</tr>
-				<?php do_action( 'wpo_wcpdf_after_order_data', $this->type, $this->order ); ?>
+				<?php do_action( 'wpo_wcpdf_after_order_data', $this->get_type(), $this->order ); ?>
 			</table>			
 		</td>
 	</tr>
 </table>
 
-<?php do_action( 'wpo_wcpdf_before_order_details', $this->type, $this->order ); ?>
+<?php do_action( 'wpo_wcpdf_before_order_details', $this->get_type(), $this->order ); ?>
 
 <table class="order-details">
 	<thead>
@@ -96,11 +104,11 @@
 	</thead>
 	<tbody>
 		<?php $items = $this->get_order_items(); if( sizeof( $items ) > 0 ) : foreach( $items as $item_id => $item ) : ?>
-		<tr class="<?php echo apply_filters( 'wpo_wcpdf_item_row_class', $item_id, $this->type, $this->order, $item_id ); ?>">
+		<tr class="<?php echo apply_filters( 'wpo_wcpdf_item_row_class', $item_id, $this->get_type(), $this->order, $item_id ); ?>">
 			<td class="product">
 				<?php $description_label = __( 'Description', 'woocommerce-pdf-invoices-packing-slips' ); // registering alternate label translation ?>
 				<span class="item-name"><?php echo $item['name']; ?></span>
-				<?php do_action( 'wpo_wcpdf_before_item_meta', $this->type, $item, $this->order  ); ?>
+				<?php do_action( 'wpo_wcpdf_before_item_meta', $this->get_type(), $item, $this->order  ); ?>
 				<div class="item-meta"><?php echo $item['meta']; ?></div>
 				<ul class="meta">
 					<?php $description_label = __( 'SKU', 'woocommerce-pdf-invoices-packing-slips' ); // registering alternate label translation ?>
@@ -111,7 +119,7 @@
 						<li class="weight"><strong class="weight-label"><?php _e( 'Weight:', 'woocommerce-pdf-invoices-packing-slips' ); ?></strong> <?php echo $item['weight']; ?><?php echo get_option('woocommerce_weight_unit'); ?></li>
 					<?php endif; ?>
 				</ul>
-				<?php do_action( 'wpo_wcpdf_after_item_meta', $this->type, $item, $this->order  ); ?>
+				<?php do_action( 'wpo_wcpdf_after_item_meta', $this->get_type(), $item, $this->order  ); ?>
 			</td>
 			<td class="quantity"><?php echo $item['quantity']; ?></td>
 			<td class="price"><?php echo $item['order_price']; ?></td>
@@ -158,13 +166,18 @@
 	</tbody>
 </table>
 
-<?php do_action( 'wpo_wcpdf_after_order_details', $this->type, $this->order ); ?>
+<?php do_action( 'wpo_wcpdf_after_order_details', $this->get_type(), $this->order ); ?>
+
+<div class="bottom-spacer"></div>
 
 <?php if ( $this->get_footer() ): ?>
-<div id="footer">
-	<!-- hook available: wpo_wcpdf_before_footer -->
-	<?php $this->footer(); ?>
-	<!-- hook available: wpo_wcpdf_after_footer -->
-</div><!-- #letter-footer -->
+	<htmlpagefooter name="docFooter">
+		<div id="footer">
+			<!-- hook available: wpo_wcpdf_before_footer -->
+			<?php $this->footer(); ?>
+			<!-- hook available: wpo_wcpdf_after_footer -->
+		</div><!-- #letter-footer -->
+	</htmlpagefooter>
 <?php endif; ?>
-<?php do_action( 'wpo_wcpdf_after_document', $this->type, $this->order ); ?>
+
+<?php do_action( 'wpo_wcpdf_after_document', $this->get_type(), $this->order ); ?>
