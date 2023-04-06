fffa6553b969d68d9888d923c51835fe65cf198b
Alexandre Faustino <alexmigf@gmail.com>
2023-04-06 09:58:30 +0100

    v2.4.2

--- wcpdf-mpdf.php
+++ wcpdf-mpdf.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name: WooCommerce PDF Invoices & Packing Slips + mPDF
- * Plugin URI: https://github.com/wpovernight/woocommerce-pdf-ips-mpdf/
+ * Plugin URI:  https://github.com/wpovernight/woocommerce-pdf-ips-mpdf/
  * Description: Uses mPDF instead of dompdf for HTML to PDF conversion
- * Version: 2.4.1
- * Author: WP Overnight
- * Author URI: https://www.wpovernight.com
- * License: GPLv2 or later
+ * Version:     2.4.2
+ * Author:      WP Overnight
+ * Author URI:  https://www.wpovernight.com
+ * License:     GPLv2 or later
  * License URI: https://opensource.org/licenses/gpl-license.php
  * Text Domain: woocommerce-pdf-invoices-packing-slips
  */
